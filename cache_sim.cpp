334065c6011bdcd4d2e4d7176176174a3d0dd445
Vincent SIv <133035256+VinnyBitties@users.noreply.github.com>
2026-04-17 23:27:40 -0500

    Update cache_sim.cpp

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,229 +1,245 @@
 #include<iostream>
 #include<fstream>
-#include<string>
+#include<cstdlib>
 
 using namespace std;
 
-// One cache entry represents one line in cache memory
 class Entry {
 public:
-    Entry() {
-        // Start with an empty line
-        valid = false;
-        tag = 0;
-        ref = 0;
-    }
+  Entry();
+  ~Entry();
+  void display(ofstream& outfile);
 
-    ~Entry() {
-    }
+  void set_tag(int _tag) { tag = _tag; }
+  int get_tag() { return tag; }
 
-    // Print entry contents if needed
-    void display(ofstream& outfile) {
-        outfile << valid << " " << tag << " " << ref;
-    }
+  void set_valid(bool _valid) { valid = _valid; }
+  bool get_valid() { return valid; }
 
-    void set_tag(int _tag) { tag = _tag; }
-    int get_tag() { return tag; }
-
-    void set_valid(bool _valid) { valid = _valid; }
-    bool get_valid() { return valid; }
-
-    void set_ref(int _ref) { ref = _ref; }
-    int get_ref() { return ref; }
+  void set_ref(int _ref) { ref = _ref; }
+  int get_ref() { return ref; }
 
 private:
-    bool valid;     // tells if this line contains data
-    unsigned tag;   // used to identify stored address
-    int ref;        // used for replacement tracking
+  bool valid;
+  unsigned tag;
+  int ref;
 };
 
-// Cache class controls all cache operations
 class Cache {
 public:
-    Cache(int _num_entries, int _assoc) {
-        assoc = _assoc;
-        num_entries = _num_entries;
+  Cache(int, int);
+  ~Cache();
 
-        // Number of sets = total lines / lines per set
-        num_sets = num_entries / assoc;
+  void display(ofstream& outfile);
 
-        // Build 2D dynamic array
-        // rows = ways
-        // columns = sets
-        entries = new Entry*[assoc];
+  int get_index(unsigned long addr);
+  int get_tag(unsigned long addr);
 
-        for (int i = 0; i < assoc; i++) {
-            entries[i] = new Entry[num_sets];
-        }
-    }
+  unsigned long retrieve_addr(int way, int index);
 
-    ~Cache() {
-        // Free memory when program ends
-        for (int i = 0; i < assoc; i++) {
-            delete[] entries[i];
-        }
+  bool hit(ofstream& outfile, unsigned long addr);
 
-        delete[] entries;
-    }
-
-    // Optional function to print cache contents
-    void display(ofstream& outfile) {
-        for (int i = 0; i < assoc; i++) {
-            for (int j = 0; j < num_sets; j++) {
-                outfile << "Way " << i
-                        << ", Set " << j << ": ";
-                entries[i][j].display(outfile);
-                outfile << endl;
-            }
-        }
-    }
-
-    // Finds which set an address belongs to
-    int get_index(unsigned long addr) {
-        return addr % num_sets;
-    }
-
-    // Finds the tag for an address
-    int get_tag(unsigned long addr) {
-        return addr / num_sets;
-    }
-
-    // Rebuild original address from tag + set index
-    unsigned long retrieve_addr(int way, int index) {
-        return (entries[way][index].get_tag() * num_sets) + index;
-    }
-
-    // Checks if address already exists in cache
-    bool hit(ofstream& outfile, unsigned long addr) {
-        int index = get_index(addr);
-        int tag_val = get_tag(addr);
-
-        // Search every way in this set
-        for (int i = 0; i < assoc; i++) {
-
-            // Valid line + matching tag = HIT
-            if (entries[i][index].get_valid() &&
-                entries[i][index].get_tag() == tag_val) {
-
-                outfile << addr << " : HIT" << endl;
-                return true;
-            }
-        }
-
-        // Address not found
-        outfile << addr << " : MISS" << endl;
-        return false;
-    }
-
-    // Adds new address after a miss
-    void update(ofstream& outfile, unsigned long addr) {
-        int index = get_index(addr);
-        int tag_val = get_tag(addr);
-
-        // First look for an empty line
-        for (int i = 0; i < assoc; i++) {
-
-            if (!entries[i][index].get_valid()) {
-
-                // Store new block here
-                entries[i][index].set_valid(true);
-                entries[i][index].set_tag(tag_val);
-                entries[i][index].set_ref(0);
-                return;
-            }
-        }
-
-        // If no empty space exists,
-        // replace the line with lowest ref value
-        int victim = 0;
-        int lowest_ref = entries[0][index].get_ref();
-
-        for (int i = 1; i < assoc; i++) {
-
-            if (entries[i][index].get_ref() < lowest_ref) {
-                lowest_ref = entries[i][index].get_ref();
-                victim = i;
-            }
-        }
-
-        // Replace chosen line
-        entries[victim][index].set_valid(true);
-        entries[victim][index].set_tag(tag_val);
-        entries[victim][index].set_ref(0);
-
-        // Increase age of all other lines
-        for (int i = 0; i < assoc; i++) {
-            if (i != victim) {
-                entries[i][index].set_ref(
-                    entries[i][index].get_ref() + 1
-                );
-            }
-        }
-    }
+  void update(ofstream& outfile, unsigned long addr);
 
 private:
-    int assoc;              // lines per set
-    unsigned num_entries;  // total lines
-    int num_sets;          // total sets
-    Entry **entries;       // 2D cache array
+  int assoc;
+  unsigned num_entries;
+  int num_sets;
+  Entry **entries;
+
+
+
+  int *replacement_index;
 };
+
+
+
+
+
+Entry::Entry() {
+  valid = false;
+  tag = 0;
+  ref = 0;
+}
+
+Entry::~Entry() {
+}
+
+//Helper func for debugging-> cahce contents
+void Entry::display(ofstream& outfile) {
+  outfile << "Valid: " << valid
+          << " Tag: " << tag
+          << " Ref: " << ref << endl;
+}
+
+
+
+
+// Build the cache as a 2D dynamic array.
+//
+// We are storing the cache as:
+// entries[way][set]
+//
+// - first index tells us which line/way inside the set
+// - second index tells us which set
+Cache::Cache(int _num_entries, int _assoc) {
+  num_entries = _num_entries;
+  assoc = _assoc;
+  num_sets = num_entries / assoc;
+
+  entries = new Entry*[assoc];
+  for (int i = 0; i < assoc; i++) {
+    entries[i] = new Entry[num_sets];
+  }
+
+  replacement_index = new int[num_sets];
+  for (int i = 0; i < num_sets; i++) {
+    replacement_index[i] = 0;
+  }
+}
+
+Cache::~Cache() {
+  for (int i = 0; i < assoc; i++) {
+    delete[] entries[i];
+  }
+  delete[] entries;
+
+  delete[] replacement_index;
+}
+
+void Cache::display(ofstream& outfile) {
+  for (int set = 0; set < num_sets; set++) {
+    outfile << "Set " << set << ":" << endl;
+    for (int way = 0; way < assoc; way++) {
+      outfile << "  Way " << way << " -> ";
+      entries[way][set].display(outfile);
+    }
+  }
+}
+
+// Finds which set the address belongs to.
+int Cache::get_index(unsigned long addr) {
+  return addr % num_sets;
+}
+
+// Finds the tag for the address.
+// After removing the set portion, the remaining upper part is the tag.
+int Cache::get_tag(unsigned long addr) {
+  return addr / num_sets;
+}
+
+// Rebuilds an address from a stored cache entry.
+unsigned long Cache::retrieve_addr(int way, int index) {
+  return entries[way][index].get_tag() * num_sets + index;
+}
+
+// Checks whether the given address is already stored in the correct set.
+// If found, it is a HIT. Otherwise, it is a MISS.
+bool Cache::hit(ofstream& outfile, unsigned long addr) {
+  int index = get_index(addr);
+  int tag = get_tag(addr);
+
+  // Search every way in the correct set
+  for (int way = 0; way < assoc; way++) {
+    if (entries[way][index].get_valid() &&
+        entries[way][index].get_tag() == tag) {
+      outfile << addr << " : HIT" << endl;
+      return true;
+    }
+  }
+
+  outfile << addr << " : MISS" << endl;
+  return false;
+}
+
+// Miss function
+// It adds the new address into the cache.
+//
+// Step 1: Try to place it in an empty line in that set.
+// Step 2: If the set is already full, replace one line using round-robin.
+void Cache::update(ofstream& outfile, unsigned long addr) {
+  int index = get_index(addr);
+  int tag = get_tag(addr);
+
+  // First try to find an unused line in this set
+  for (int way = 0; way < assoc; way++) {
+    if (!entries[way][index].get_valid()) {
+      entries[way][index].set_valid(true);
+      entries[way][index].set_tag(tag);
+      return;
+    }
+  }
+
+  // If set is full.
+  // Replace the line pointed to by replacement_index[index].
+  int victim = replacement_index[index];
+
+  entries[victim][index].set_valid(true);
+  entries[victim][index].set_tag(tag);
+
+  // Move to the next line in this set for future replacement
+  replacement_index[index] = (replacement_index[index] + 1) % assoc;
+}
+
+
 
 int main(int argc, char* argv[]) {
 
-    // Program must be run like:
-    // ./cache_sim 4 2 input0.txt
-    if (argc != 4) {
-        cerr << "Usage: ./cache_sim <num_entries> "
-             << "<associativity> <input_file>" << endl;
-        return 1;
-    }
 
-    // Read command line values
-    int num_entries = stoi(argv[1]);
-    int associativity = stoi(argv[2]);
-    string input_file_name = argv[3];
-
-    // Make sure cache setup is valid
-    if (num_entries <= 0 ||
-        associativity <= 0 ||
-        num_entries % associativity != 0) {
-
-        cerr << "Invalid cache configuration." << endl;
-        return 1;
-    }
-
-    // Open file containing memory addresses
-    ifstream input_file(input_file_name);
-
-    if (!input_file.is_open()) {
-        cerr << "Could not open input file." << endl;
-        return 1;
-    }
-
-    // Create output file
-    ofstream output_file("cache_sim_output");
-
-    if (!output_file.is_open()) {
-        cerr << "Could not create output file." << endl;
-        return 1;
-    }
-
-    // Build cache object
-    Cache cache(num_entries, associativity);
-
-    unsigned long address;
-
-    // Read one address at a time
-    while (input_file >> address) {
-
-        // If not found, place it into cache
-        if (!cache.hit(output_file, address)) {
-            cache.update(output_file, address);
-        }
-    }
-
-    input_file.close();
-    output_file.close();
-
+  if (argc < 4) {
+    cout << "Usage: " << endl;
+    cout << "   ./cache_sim num_entries associativity input_file" << endl;
     return 0;
+  }
+
+
+  unsigned entries = atoi(argv[1]);
+  unsigned assoc = atoi(argv[2]);
+  string input_filename = argv[3];
+
+
+  if (entries == 0 || assoc == 0 || entries % assoc != 0) {
+    cerr << "Invalid cache configuration." << endl;
+    return 1;
+  }
+
+  // Open input file
+  ifstream input;
+  input.open(input_filename.c_str());
+
+  if (!input.is_open()) {
+    cerr << "Could not open input file " << input_filename << "." << endl;
+    return 1;
+  }
+
+  // Open output file
+  ofstream output;
+  output.open("cache_sim_output");
+
+  if (!output.is_open()) {
+    cerr << "Could not create output file." << endl;
+    return 1;
+  }
+
+  // Build cache using command-line configuration
+  Cache cache(entries, assoc);
+
+  unsigned long addr;
+
+  // Read one memory reference at a time from the input file
+  while (input >> addr) {
+
+    // First check whether this address is already in cache
+    bool found = cache.hit(output, addr);
+
+    // If it was not found, insert it into cache
+    if (!found) {
+      cache.update(output, addr);
+    }
+  }
+
+  input.close();
+  output.close();
+
+  return 0;
 }
